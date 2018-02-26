7c99b82e6554ac243420384a254d8d8057c9c36f
KrogRR <35690733+KrogRR@users.noreply.github.com>
2018-02-26 16:46:43 +0900

    Add files via upload

--- bootstrap/js/width.js
+++ bootstrap/js/width.js
@@ -1,8 +1,8 @@
 function col_md_9_width() {
     var titleWidth = parseInt(d3.select("#title-width").style("width").split("p")[0]);
-	console.log(titleWidth+"title");
+	//console.log(titleWidth+"title");
 	titleWidth=titleWidth-230;
-	console.log(titleWidth+"mainbody");
+	//console.log(titleWidth+"mainbody");
 	d3.select("#mainbody-width").style("width",""+titleWidth+"px");
 }
 	window.onresize = function(){
