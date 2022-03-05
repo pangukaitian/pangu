8f984170ed77f4e7bca5de9d3e5d60caa5164e44
MamoruDS <mamoruds.io@gmail.com>
2022-03-05 22:58:09 +0900

    fixed: old publish parse issue

--- old/pub.ts
+++ old/pub.ts
@@ -24,34 +24,72 @@ const run = async (): Promise<void> => {
         '#publications > * li'
     ) as NodeListOf<HTMLElement>
     node_iter(pubs, (pub) => {
-        if (pub.textContent.replace(/\s/g, '')) {
-            node_iter(pub.querySelectorAll('a'), (anchor) => {
-                const url = anchor.getAttribute('href')
-                anchor.replaceWith(
-                    `[${anchor.textContent.replace(/\s+/g, ' ')}](${url})`
-                )
-            })
-            const lns = pub.textContent
-                .split('\n')
-                .map((t) => t.replace(/^\s+/g, ''))
-                .filter((t) => t)
-            const author = lns.shift()
-            const authors = author
-                .replace(/[,|\s]+and\s+/g, ',')
-                .split(',')
-                .map((a) => {
-                    if (typeof a == 'string')
-                        return a.replace(/(^\s+)|([\.|\s]{0,}$)/g, '')
-                })
-                .filter((a) => a)
-            const title = lns.shift()
-            const publish = lns.join(' ')
-            res.push({
-                title,
-                authors,
-                publish,
-            })
+        const nodes = pub.childNodes
+        const order = ['authors', 'title', 'publish']
+        let child_i = 0
+        let authors: string[]
+        let title: string = ''
+        let url: undefined | string = undefined
+        let publish: string = ''
+
+        if (!pub.textContent.replace(/\s/g, '')) {
+            return
         }
+        while (order.length) {
+            const node = nodes[child_i]
+            if (typeof nodes[child_i] == 'undefined') {
+                break
+            }
+            child_i++
+            if (node.textContent.replace(/\s/g, '')) {
+                const cur = order[0]
+                if (cur == 'authors') {
+                    authors = node.textContent
+                        .replace(/[,|\s]+and\s+/g, ',')
+                        .split(',')
+                        .map((a) => {
+                            if (typeof a == 'string')
+                                return a.replace(/(^\s+)|([\.|\s]{0,}$)/g, '')
+                        })
+                        .filter((a) => a)
+                    order.shift()
+                    continue
+                }
+                if (cur == 'title') {
+                    if (node.nodeType == 1) {
+                        const a = node as Element
+                        url = a.getAttribute('href')
+                        title = a.textContent
+                            .replace(/^\s+/gm, '')
+                            .replace(/\n/g, ' ')
+                            .replace(/\s+$/g, '')
+                    } else {
+                        title = nodes[2].textContent
+                            .replace(/^\s+/gm, '')
+                            .replace(/\n/g, ' ')
+                            .replace(/\s+$/g, '')
+                    }
+                    order.shift()
+                    continue
+                }
+                if (cur == 'publish') {
+                    publish += node.textContent
+                        .replace(/^\s+/gm, '')
+                        .replace(/\n/g, ' ')
+                        .replace(/\s+$/g, '')
+                    if (typeof nodes[child_i] == 'undefined') {
+                        order.shift()
+                    }
+                    continue
+                }
+            }
+        }
+        res.push({
+            title,
+            url,
+            authors,
+            publish,
+        })
     })
     console.log(yaml.dump(res, { indent: 4 }))
 }
